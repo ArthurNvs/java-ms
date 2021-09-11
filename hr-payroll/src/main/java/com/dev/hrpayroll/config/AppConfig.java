6880c742428b592fced5570c48fcddaba98d1d28
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-09-11 01:23:09 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/dev/hrpayroll/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.dev.hrpayroll.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    //Singleton
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
