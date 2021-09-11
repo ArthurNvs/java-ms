d18c36a4262ede51616fb847af20cb3e2a03136a
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-09-11 00:23:05 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Davi', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Flavia', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Selma', 250.0);
