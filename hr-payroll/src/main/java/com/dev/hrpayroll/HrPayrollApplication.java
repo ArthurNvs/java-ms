02e30210e0baaafc5dbe30a13837cdc1a069ecb9
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-09-11 16:23:04 -0300

    Configuring eureka clients

--- hr-payroll/src/main/java/com/dev/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/dev/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,12 @@ package com.dev.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
+//Eureka já faz balanceamento de carga
+//@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
