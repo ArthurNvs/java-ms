c80eec458f5de6353728ddbe5a42f2e81289ce74
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-09-11 23:19:46 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/dev/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/dev/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,11 @@ package com.dev.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 //Eureka já faz balanceamento de carga
 //@RibbonClient(name = "hr-worker")
